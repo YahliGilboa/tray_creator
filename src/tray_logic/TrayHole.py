4705e97fe4ac934215211d63338585c6e064e6d0
unknown <yahli.gilboa@gmail.com>
2024-09-09 22:13:10 +0300

    added some tray hole logic, check add hole if no intersection

---
+++ src/tray_logic/TrayHole.py
@@ -0,0 +1,4 @@
+class TrayHole():
+    def __init__(self, top_left_pos, bottom_right_pos):
+        self.top_left_index_pos = top_left_pos
+        self.bottom_right_index_pos = bottom_right_pos
--- src/tray_logic/traycontainer.py
+++ src/tray_logic/traycontainer.py
@@ -1,18 +1,19 @@
-# from TrayCell import TrayCell
-# import math
+from TrayHole import TrayHole
 import src.tray_logic.conf as conf
 
 
 class TrayContainer():
     # this is cell clumps, but ill call it hole because this will generate it
-    tray_holes = []
+    tray_holes: list[TrayHole] = []
     X_Cells = 0
     Y_Cells = 0
 
-    def __init__(self, width_in_mm, length_in_mm, height_in_mm=conf.DEFAULT_TRAY_HEIGHT_MM):
+    def __init__(self, width_in_mm, length_in_mm, height_in_mm=conf.DEFAULT_TRAY_HEIGHT_MM,
+                 wall_thickness_in_mm=conf.DEFAULT_TRAY_WALL_THICKNESS_MM):
         self.width_in_mm = width_in_mm
         self.length_in_mm = length_in_mm
         self.height_in_mm = height_in_mm
+        self.wall_thickness_in_mm = wall_thickness_in_mm
         self.X_Cells, self.Y_Cells = self.calculate_cell_matrix(width_in_mm, length_in_mm)
 
     def calculate_cell_matrix(self, width_in_mm, length_in_mm):
@@ -38,7 +39,23 @@ class TrayContainer():
             else:
                 return small_side_cells, big_side_cells
 
-    # def add_hole(self):
+    def add_hole(self, tray_hole: TrayHole):
+        if not self.tray_hole_intersect_existing(tray_hole):
+            self.tray_holes.append(tray_hole)
 
-# def generate_matrix_from_width_height(self,width,height):
+    def tray_hole_intersect_existing(self, tray_hole: TrayHole) -> bool:
+        tray_hole_intersect = False
+        for hole in self.tray_holes:
+            x_overlap = tray_hole.top_left_index_pos[0] >= hole.top_left_index_pos[0] or \
+                        tray_hole.bottom_right_index_pos[0] <= hole.bottom_right_index_pos[0]
+
+            y_overlap = tray_hole.top_left_index_pos[1] >= hole.top_left_index_pos[1] or \
+                        tray_hole.bottom_right_index_pos[1] <= hole.bottom_right_index_pos[1]
+
+            if x_overlap and y_overlap:
+                tray_hole_intersect = True
+
+        return tray_hole_intersect
+
+    # def generate_matrix_from_width_height(self,width,height):
 #     return [[Tray_Cell()] * int(width)] * int(height)
